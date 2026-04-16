7d8c8e814afd4a9b40c9d8b9765ba3a2a513d9e0
Aarsan2365 <nikarsan236@gmail.com>
2026-04-17 00:06:43 +0530

    Add wishlist icon to NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -71,6 +71,15 @@ const handleSearch = () => {
           Home
         </RouterLink>
 
+        <!-- Wishlist Button -->
+        <RouterLink 
+          to="/wishlist" 
+          class="relative p-2 rounded-xl text-zinc-600 dark:text-zinc-300 hover:bg-zinc-100 dark:hover:bg-zinc-800 transition-all hover:text-pink-500 dark:hover:text-pink-400 group"
+          title="View Wishlist"
+        >
+           <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/></svg>
+        </RouterLink>
+
         <!-- Cart Button -->
         <button 
           @click="toggleCart" 
